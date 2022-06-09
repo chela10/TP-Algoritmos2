6b8f4532bfed64587681ad0964387325d6c2ed94
Franco Losardo <francolosa@hotmail.com>
2022-06-09 15:24:23 -0300

    agrego atributos que faltaban en los constructores de las hijas de terminal

--- src/Aeropuerto.java
+++ src/Aeropuerto.java
@@ -1,8 +1,14 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Aeropuerto extends Terminal {
-    public Aeropuerto(Ciudad ciudad) {
+    public Aeropuerto(String nombre, String direccion, String telefono, int maxPasajeros, Ciudad ciudad, List<Ruta> rutas) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.maxPasajeros = maxPasajeros;
         this.ciudad = ciudad;
+        this.rutas = rutas;
         this.TransporteSoportado = new ArrayList<String>();
         this.TransporteSoportado.add("Avion");
         this.TransporteSoportado.add("MCD");
--- src/Terrestre.java
+++ src/Terrestre.java
@@ -1,8 +1,14 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Terrestre extends Terminal {
-    public Terrestre(Ciudad ciudad) {
+    public Terrestre(String nombre, String direccion, String telefono, int maxPasajeros ,Ciudad ciudad, List<Ruta> rutas) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.maxPasajeros = maxPasajeros;
         this.ciudad = ciudad;
+        this.rutas = rutas;
         this.TransporteSoportado = new ArrayList<String>();
         this.TransporteSoportado.add("Tren");
         this.TransporteSoportado.add("MCD");
